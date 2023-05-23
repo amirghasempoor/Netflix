1e3fb116ef6d70ec2b4e9cacb93a623dda7c5250
amirghasempoor <amirghasempoor79@gmail.com>
2023-05-23 15:40:56 +0330

    create role controller, api, requests

---
+++ app/Http/Controllers/RoleManagement/RoleController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\RoleManagement;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Role\StoreRequest;
+use App\Http\Requests\Role\UpdateRequest;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'roles' => Role::orderBy('id', 'asc')->with('permissions')
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $role = Role::create([
+            'name' => $request->name
+        ]);
+        
+        $role->givePermissionTo($request->permission_id);
+
+        return response()->json([
+            'message' => 'created successfully',
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Role $role)
+    {
+        return response()->json([
+            'role' => $role->load('permissions'),
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, Role $role)
+    {
+        $role->update([
+            'name' => $request->name
+        ]);
+
+        $role->syncPermissions($request->permission_id);
+
+        return response()->json([
+            'message' => 'updated successfully'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Role $role)
+    {
+        $role->syncPermissions([]);
+
+        $role->delete();
+
+        return response()->json([
+            'message' => 'deleted successfully'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleManagement\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/user_register', [AuthController::class, 'userRegister']);
 Route::post('/user_login', [AuthController::class, 'userLogin']);
 Route::post('/user_logout', [AuthController::class, 'userLogout'])->middleware('auth:sanctum');
+
+Route::prefix('roles')->group(function() {
+    Route::get('/', [RoleController::class, 'index'])->middleware('auth:sanctum');
+    Route::post('/', [RoleController::class, 'store'])->middleware('auth:sanctum');
+    Route::get('/{role}', [RoleController::class, 'show'])->where('role', '[0-9]+')->middleware('auth:sanctum');
+    Route::put('/{role}', [RoleController::class, 'update'])->where('role', '[0-9]+')->middleware('auth:sanctum');
+    Route::delete('/{role}', [RoleController::class, 'destroy'])->where('role', '[0-9]+')->middleware('auth:sanctum');
+});
